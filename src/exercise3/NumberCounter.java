c123f64dc43c9ba4810f25569de32d612c61ca34
Deyvid <>
2021-10-21 06:17:00 +0000

    1.01.01

--- src/exercise3/NumberCounter.java
+++ src/exercise3/NumberCounter.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class NumberCounter {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    int sum = 0;
+    double sum = 0;
     int positiveCounter = 0;
     int negativeCounter = 0;
     double average = 0;
