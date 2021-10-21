710ba53e7fbbbead3c8ca11d74a9650e4fc480e9
Deyvid <>
2021-10-21 20:58:38 +0000

    6.01.00

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -1,14 +1,21 @@
 package exercise3;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class AngleCalculation {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
-        char type = input.next().charAt(0);
+        boolean flag = true;
 
-        System.out.println(type);
+        do{
+          char type = input.next().charAt(0);
+          int grad = input.nextInt();
+          if(type == 'd') {
+            double r = grad*Math.PI/180;
+            System.out.println("Angle: " + r + "r");
+          }
+        }while(flag);
     }
 }
