9244bba0a96131a9f9d091faad2ac0f137ccdd22
Deyvid <>
2021-10-21 20:43:38 +0000

    5.00.00

---
+++ src/exercise3/AngleCalculation.java
@@ -0,0 +1,14 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class AngleCalculation {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        char type = input.nextChar();
+
+        System.out.println(type);
+    }
+}
