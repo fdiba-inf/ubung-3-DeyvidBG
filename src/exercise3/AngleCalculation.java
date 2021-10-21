060e743d2be53cae3bf3c50255745f490c49e7af
Deyvid <>
2021-10-21 21:09:54 +0000

    6.03.00

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -10,14 +10,16 @@ public class AngleCalculation {
         boolean flag = true;
 
         do{
-          char type = input.next().charAt(0);
           double grad = input.nextInt();
+          char type = input.next().charAt(0);
           if(type == 'd') {
             double r = grad*Math.PI/180;
-            System.out.println("Angle: " + r + "r");
+            System.out.println("Angle: " + String.format("%.2f", r));
           } else if(type == 'r') {
             double d =  grad*180/Math.PI;
-            System.out.println("Angle: " + d + "d");
+            System.out.println("Angle: " + String.format("%.2f", d));
+          } else {
+            flag = !flag;
           }
         }while(flag);
     }
