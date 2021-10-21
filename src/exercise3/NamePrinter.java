d48281ef6ac130ccb589707947086d7619838109
Deyvid <>
2021-10-21 06:41:57 +0000

    4.00.01

--- src/exercise3/NamePrinter.java
+++ src/exercise3/NamePrinter.java
@@ -10,7 +10,7 @@ public class NamePrinter {
         String name = input.nextLine();
 
         for(int i = 0; i < name.length(); i++) {
-          System.out.println("*" + name.charAt(i) + "*");
+          System.out.println("* " + name.charAt(i) + " *");
         }
     }
 }
